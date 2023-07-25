e503fb58c4fd38e4059ea986970a7de2d02e1c7d
Omar Garcia <omar.garcia@omargl.net>
2023-07-25 11:53:15 -0600

    Cargando html template y formulario

--- 03_consumiendoAPI/app/main.js
+++ 03_consumiendoAPI/app/main.js
@@ -1,1 +1,37 @@
-console.log('Main JS cargado con éxito')
+const loadInitialTemplate = () => {
+    // Template string
+    const template = `
+        <h1>Usuarios</h1>
+        <form id="user-form">
+            <div>
+                <label>Nombre</label>
+                <input name="name" />
+            </div>
+            <div>
+                <label>Apellido</label>
+                <input name="lastname" />
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+        <ul id="user-list"></ul>
+    `
+    // Adjuntamos el html dentro de la etiqueta de body
+    const body = document.getElementsByTagName('body')[0]
+    body.innerHTML = template
+}
+
+const addFormListener = () => {
+    const userForm = document.getElementById('user-form')
+    userForm.onsubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(userForm)
+        // Pasar los valores de un formulario a un objeto
+        const data = Object.fromEntries(formData.entries())
+        console.log(data)
+    }
+}
+
+window.onload = () => {
+    loadInitialTemplate ()
+    addFormListener()
+}
