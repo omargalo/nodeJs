a7aeb9b024abcf064a4c503acbdcb58e28baf243
Omar Garcia <omar.garcia@omargl.net>
2023-08-01 13:46:37 -0600

    Protegiendo endpoints

--- 11_animales/animal.model.js
+++ 11_animales/animal.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose') 
+const mongoose = require('mongoose')
 
 
 const Animals = mongoose.model('Animal', {
--- 11_animales/api.js
+++ 11_animales/api.js
@@ -2,17 +2,22 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const Animal = require('./animal.controller')
+// Object destructuring
+const { Auth, isAuthenticated } = require('./auth.controller')
 const port = 3000
 
-mongoose.connect('mongodb://localhost:27017/miapp')
+mongoose.connect('mongodb+srv://omargarcialopez13:chips2023@clusteroga.iw9gwgc.mongodb.net/auth?retryWrites=true&w=majority')
 
 app.use(express.json())
 
-app.get('/animals', Animal.list)
-app.post('/animals', Animal.create)
-app.put('/animals/:id', Animal.update)
-app.patch('/animals/:id', Animal.update)
-app.delete('/animals/:id', Animal.destroy)
+app.get('/animals', isAuthenticated, Animal.list)
+app.post('/animals', isAuthenticated, Animal.create)
+app.put('/animals/:id', isAuthenticated, Animal.update)
+app.patch('/animals/:id', isAuthenticated, Animal.update)
+app.delete('/animals/:id', isAuthenticated, Animal.destroy)
+
+app.post('/login', Auth.login)
+app.post('/register', Auth.register)
 
 app.use(express.static('app'))
 
