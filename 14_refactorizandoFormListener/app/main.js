0b01796716419e908a62740f9878e1b3ddf299e2
Omar Garcia <omar.garcia@omargl.net>
2023-08-03 11:05:58 -0600

    Refactorizando form listener

---
+++ 14_refactorizandoFormListener/app/main.js
@@ -0,0 +1,188 @@
+const loadInitialTemplate = () => {
+	const template = `
+		<h1>Animales</h1>
+		<form id="animal-form">
+			<div>
+				<label>Nombre</label>
+				<input name="name" />
+			</div>
+			<div>
+				<label>Tipo</label>
+				<input name="type" />
+			</div>
+			<button type="submit">Enviar</button>
+		</form>
+		<ul id="animal-list"></ul>
+	`
+	const body = document.getElementsByTagName('body')[0]
+	body.innerHTML = template
+}
+
+const getAnimals = async () => {
+	const response = await fetch('/animals', {
+		headers: {
+			Authorization: localStorage.getItem('jwt')
+		}
+	})
+	const animals = await response.json()
+	const template = animal => `
+		<li>
+			${animal.name} ${animal.type} <button data-id="${animal._id}">Eliminar</button>
+		</li>
+	`
+
+	const animalList = document.getElementById('animal-list')
+	animalList.innerHTML = animals.map(animal => template(animal)).join('')
+	animals.forEach(animal => {
+		animalNode = document.querySelector(`[data-id="${animal._id}"]`)
+		animalNode.onclick = async e => {
+			await fetch(`/animals/${animal._id}`, {
+				method: 'DELETE',
+				headers: {
+					Authorization: localStorage.getItem('jwt')
+				}
+			})
+			animalNode.parentNode.remove()
+			alert('Eliminado con éxito')
+		}
+	})
+}
+
+const addFormListener = () => {
+	const animalForm = document.getElementById('animal-form')
+	animalForm.onsubmit = async (e) => {
+		e.preventDefault()
+		const formData = new FormData(animalForm)
+		const data = Object.fromEntries(formData.entries())
+		await fetch('/animals', {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: localStorage.getItem('jwt')
+			}
+		})
+		animalForm.reset()
+		getAnimals()
+	}
+}
+
+// Validar que el usuario haya iniciado sesión con éxito
+const checkLogin = () =>
+	localStorage.getItem('jwt')
+
+const animalsPage = () => {
+	loadInitialTemplate()
+	addFormListener()
+  	getAnimals()
+}
+
+const loadRegisterTemplate = () => {
+	const template = `
+	<h1>Register</h1>
+	<form id="register-form">
+		<div>
+			<label>Correo</label>
+			<input name="email" />
+		</div>
+		<div>
+			<label>Contraseña</label>
+			<input type="password" name="password" />
+		</div>
+		<button type="submit">Enviar</button>
+	</form>
+	<a href="#" id="login">iniciar Sesión</a>
+	<div id="error"></div>
+`
+}
+
+const gotoLoginListener = () => {
+	const gotoLogin = document.getElementById('login')
+	gotoLogin.onclick = (e) => {
+	e.preventDefault()
+	loginPage()
+	}
+}
+
+const registerPage = () => {
+	console.log('Pagina de registro')
+	loadRegisterTemplate()
+	addRegisterListener()
+  	gotoLoginListener()
+}
+
+const loginPage = () => {
+	loadLoginTemplate()
+	addLoginListener()
+	gotoRegisterListener()
+}
+
+const loadLoginTemplate = () => {
+	const template = `
+	<h1>Login</h1>
+	<form id="login-form">
+		<div>
+			<label>Correo</label>
+			<input name="email" />
+		</div>
+		<div>
+			<label>Contraseña</label>
+			<input type="password" name="password" />
+		</div>
+		<button type="submit">Enviar</button>
+	</form>
+	<a href="#" id="register">Registrarse</a>
+	<div id="error"></div>
+`
+const body = document.getElementsByTagName('body')[0]
+body.innerHTML = template
+}
+
+const gotoRegisterListener = () => {
+	const gotoRegister = document.getElementById('register')
+	gotoRegister.onclick = (e) => {
+		e.preventDefault()
+		registerPage()
+	}
+}
+
+//Refactorizando
+const authListener = action => () => {
+	const form = document.getElementById(`${action}-form`)
+	form.onsubmit = async (e) => {
+		e.preventDefault()
+		const formData = new FormData(form)
+		const data = Object.fromEntries(formData.entries())
+		// Enviando datos al endpoint de login
+		const response = await fetch(`/${action}`, {
+			method: 'POST',
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+		// Capturando la respuesta
+		const responseData = await response.text()
+		if(response.status >= 300) {
+			const errorNode = document.getElementById('error')
+			errorNode.innerHTML = responseData
+		} else {
+			localStorage.setItem('jwt', `Bearer ${responseData}`)
+			animalsPage()
+		}
+	}
+}
+
+// Transformar los campos del formulario en un objeto JS
+const addLoginListener = authListener('login')
+const addRegisterListener = authListener('register')
+
+window.onload = () => {
+	const isLoggedIn = checkLogin()
+	if(isLoggedIn) {
+		animalsPage()
+	// Cuando el usuario no haya iniciado sesión
+	} else {
+		loginPage()
+	}
+}
